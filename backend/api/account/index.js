a238da659e8bbe8bfbc8b45071ec63a99c4f3806
SeunghanJI <jsh3581482@naver.com>
2023-03-08 15:31:17 +0900

    refactor : dbController를 knex 모듈로 대체

--- backend/api/account/index.js
+++ backend/api/account/index.js
@@ -1,73 +1,82 @@
 const express = require('express');
 const app = express();
-const dbController = require("../../dbController");
 const jwt = require("jsonwebtoken");
 
-app.get("/", (req, res) => {
-    if (req.cookies && req.cookies.token) {
-        jwt.verify(req.cookies.token, "abc1234", (err, decoded) => {
-            if (err) {
-                return res.send(401);
-            }
-            res.send(decoded);
-        });
-    } else {
-        res.send(401);
-    }
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: 'db/main.db'
+    },
+    useNullAsDefault: true
 });
 
-app.post("/", (req, res) => {
+app.get('/', (req, res) => {
+    if (!req.cookies || !req.cookies.token) {
+        res.status(401);
+    }
+
+    jwt.verify(req.cookies.token, 'abc1234', (err, decoded) => {
+        if (err) {
+            return res.status(401);
+        }
+
+        res.status(200).json(decoded);
+    });
+});
+
+app.post('/', async (req, res) => {
     const body = req.body;
     const { loginId, loginPw } = body;
 
-    dbController
-        .getUserInfo(loginId, loginPw)
-        .then((member) => {
-            // member.type = MEMBER_TYPE_MAP[member.type];
-            const options = {
-                domain: "localhost",
-                path: "/",
-                httpOnly: true,
-            };
-            const token = jwt.sign(
-                {
-                    id: member.id,
-                    name: member.name,
-                    type: member.type,
-                },
-                "abc1234",
-                {
-                    expiresIn: "30m",
-                    issuer: "corjs",
-                }
-            );
+    try {
+        const member = await knex('member').select('*').where({ id: loginId, password: loginPw }).first();
 
-            res.cookie("token", token, options);
-            res.send(member);
-        })
-        .catch((error) => {
-            console.log(error);
-            res.status(401).json(error);
-        });
+        if (!member) {
+            return res.status(401).json({ message: '없는 회원입니다.' });
+        }
+
+        const options = {
+            domain: 'localhost',
+            path: '/',
+            httpOnly: true,
+        };
+        const token = jwt.sign(
+            {
+                id: member.id,
+                name: member.name,
+                type: member.type,
+            },
+            'abc1234',
+            {
+                expiresIn: '30m',
+                issuer: 'corjs',
+            }
+        );
+
+        res.cookie('token', token, options);
+        res.status(200).json(member)
+    } catch (err) {
+        res.status(500).json({ message: '통신 오류' });
+    }
 });
 
-app.post("../signup", (req, res) => {
+app.post('../signup', async (req, res) => {
     const body = req.body;
     const { signId, signPw, signType } = body;
-    dbController
-        .insertUser(signId, signPw, signType)
-        .then((_) => {
-            res.send("회원가입 완료");
-        })
-        .catch((error) => {
-            res.status(401).json(error);
-        });
+    try {
+        await knex('member').insert({ id: signId, password: signPw, type: signType });
+
+        res.status(200).json({ isSignUp: true });
+    } catch (err) {
+        res.status(500).json({ message: '통신 오류' });
+    }
 });
 
 app.delete("/", (req, res) => {
     if (req.cookies && req.cookies.token) {
         res.clearCookie("token");
     }
+
     res.sendStatus(200);
 });
 
--- backend/api/lecture/index.js
+++ backend/api/lecture/index.js
@@ -1,34 +1,48 @@
 const express = require('express');
 const app = express();
-const dbController = require("../../dbController");
 
-app.post('/', (req, res) => {
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: 'db/main.db'
+    },
+    useNullAsDefault: true
+});
+
+app.post('/', async (req, res) => {
     const body = req.body;
 
     if (!body?.title && !body?.context && !body?.videoUrl && !body?.price) {
         return res.status(400).json({ message: '제대로 보내세오' });
     }
 
-    const { title, context, videoUrl, price } = body;
+    const { title, context, videoUrl: video_url, price, imageUrl: image_url, user } = body;
 
-    dbController.insertLecture(title, context, videoUrl, price)
-        .then((_) => {
-            res.status(200).json({ isUploaded: true })
-        })
-        .catch((error) => {
-            return res.status(500).json(error);
-        });
+    try {
+        await knex('lecuture').insert({ title, context, video_url, price, image_url, user });
+        res.status(200).json({ isUploaded: true });
+    } catch (err) {
+        res.status(500).json({ message: '통신 오류' });
+    }
+
+    // dbController.insertLecture(title, context, videoUrl, price)
+    //     .then((_) => {
+    //         res.status(200).json()
+    //     })
+    //     .catch((error) => {
+    //         return res.status(500).json(error);
+    //     });
 
 });
 
-app.get('/list', (req, res) => {
-    dbController.getLectureList()
-        .then((lectureList) => {
-            res.status(200).json({ lectureList });
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        })
+app.get('/list', async (req, res) => {
+
+    try {
+        const lectureList = await knex('lecture').select('*');
+        res.status(200).json({ lectureList });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = app;
