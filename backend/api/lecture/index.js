817c1482d56847de25d96328600024966e9b5e87
SeunghanJI <jsh3581482@naver.com>
2023-03-10 02:33:09 +0900

    fix : 강의 상세정보 요청시 api DB쿼리문 수정 - 현재 강의 신청여부 확인시 where문 수정

--- backend/api/lecture/index.js
+++ backend/api/lecture/index.js
@@ -15,32 +15,6 @@ const knex = require('knex')({
     useNullAsDefault: true
 });
 
-app.post('/', async (req, res) => {
-    const body = req.body;
-
-    if (!body?.title || !body?.context || !body?.videoUrl || !body?.price) {
-        return res.status(400).json({ message: '제대로 보내세오' });
-    }
-
-    const { title, context, videoUrl: video_url, price, imgUrl: image_url, user } = body;
-
-    try {
-        await knex('lecture').insert({ title, context, video_url, price, image_url, user });
-        res.status(200).json({ isUploaded: true });
-    } catch (err) {
-        res.status(500).json({ message: '통신 오류' });
-    }
-});
-
-app.get('/list', async (req, res) => {
-    try {
-        const lectureList = await knex('lecture').select('*');
-        res.status(200).json({ lectureList });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 app.get('/', async (req, res) => {
     const cookies = req.cookies;
     const token = cookies?.token;
@@ -65,14 +39,39 @@ app.get('/', async (req, res) => {
     try {
         const [lectureInfo, userLecture] = await Promise.all([
             knex('lecture').select('*').where({ id: lectureId }).first(),
-            knex('student_courses').select('lecture_id').where({ user_id: user.id }).first()
+            knex('student_courses').select('lecture_id').where({ lecture_id: lectureId, user_id: user.id }).first()
         ]);
-
         const isEnrolled = !!userLecture;
 
         res.status(200).json({ ...lectureInfo, isEnrolled });
     } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+app.get('/list', async (req, res) => {
+    try {
+        const lectureList = await knex('lecture').select('*');
+        res.status(200).json({ lectureList });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+app.post('/', async (req, res) => {
+    const body = req.body;
+
+    if (!body?.title || !body?.context || !body?.videoUrl || !body?.price) {
+        return res.status(400).json({ message: '제대로 보내세오' });
+    }
+
+    const { title, context, videoUrl: video_url, price, imgUrl: image_url, user } = body;
+
+    try {
+        await knex('lecture').insert({ title, context, video_url, price, image_url, user });
+        res.status(200).json({ isUploaded: true });
+    } catch (err) {
+        res.status(500).json({ message: '통신 오류' });
     }
 });
 
