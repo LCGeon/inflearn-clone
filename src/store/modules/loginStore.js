2441a0025ebf974a7703b3bdeb90c412874ef67b
LCGeon <corjs8449@naver.com>
2023-03-16 16:13:24 +0900

    Style: 로그인 상태를 관리하는 데이터들 모듈화

---
+++ src/store/modules/loginStore.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import router from "@/router";
+const loginStore = {
+  namespaced: true,
+  state: {
+    userState: {
+      account: {
+        id: null,
+        name: "",
+        type: "",
+      },
+      form: {
+        loginId: "",
+        loginPw: "",
+      },
+    },
+    loadingStatus: false,
+    ismodal: false,
+    pwdValue: true,
+    pwd: "password",
+  },
+  mutations: {
+    logout(state) {
+      axios.delete("/api/account").then(() => {
+        state.userState.account.id = null;
+        state.userState.account.name = "";
+        state.userState.account.type = "";
+      });
+    },
+    submit(state) {
+      state.loadingStatus = true;
+      const args = {
+        loginId: state.userState.form.loginId,
+        loginPw: state.userState.form.loginPw,
+      };
+      axios
+        .post("/api/account", args)
+        .then((res) => {
+          state.userState.account = res.data;
+          state.loadingStatus = false;
+          state.ismodal = false;
+          router.push({ name: "home" });
+        })
+        .catch(() => {
+          alert("올바른 정보가 아닙니다.");
+          state.loadingStatus = false;
+        });
+    },
+
+    loginCheck(state) {
+      if (state.userState.account.id === null) {
+        alert("로그인이 필요합니다.");
+        router.push({ name: "home" });
+        state.ismodal = true;
+      }
+    },
+    pwdHide(state) {
+      state.pwd = "";
+      state.pwdValue = false;
+    },
+    pwdShow(state) {
+      state.pwd = "password";
+      state.pwdValue = true;
+    },
+    ismodalTrue(state) {
+      state.ismodal = true;
+    },
+    ismodalFalse(state) {
+      state.ismodal = false;
+    },
+    getLoginInfo(state) {
+      axios.get("/api/account").then((res) => {
+        state.userState.account = res.data;
+      });
+    },
+  },
+};
+
+export default loginStore;
